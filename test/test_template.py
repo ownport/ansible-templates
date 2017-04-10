f427af874b13e150003ff999f9c1dcbe6c6b8a26
ownport <ownport@gmail.com>
2017-04-10 06:58:10 +0300

    [+] simple templates test

--- test/test_template.py
+++ test/test_template.py
@@ -13,3 +13,11 @@ def test_templates_instance():
     templar = Templar(variables={'a': 1})
     assert isinstance(templar, Templar)
 
+
+def test_templates_template_string_types():
+
+    templar = Templar(variables={'var1': 'test'})
+    assert templar.template('test') == 'test'
+    assert templar.template('{{var1}}') == 'test'
+    assert templar.template('var1={{var1}}') == 'var1=test'
+
